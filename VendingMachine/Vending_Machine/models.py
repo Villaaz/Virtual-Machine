418cbae082b35b8069b673a9c27be518b31a0cf6
JoseRicoK <jose.rico.ceb@immune.institute>
2023-02-09 12:27:01 +0100

    cambio4

--- VendingMachine/Vending_Machine/models.py
+++ VendingMachine/Vending_Machine/models.py
@@ -4,6 +4,7 @@ class Producto(models.Model):
     codigo_producto = models.IntegerField()
     nombre_producto = models.CharField(max_length=20, default="nombre_producto")
     precio = models.FloatField()
+    stock = models.IntegerField(default=6)
     num_comprados = models.IntegerField()
 
     
