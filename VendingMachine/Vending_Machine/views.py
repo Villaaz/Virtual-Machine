596a9c17e7a37caead1637c2719dc664e07548a1
JoseRicoK <jose.rico.ceb@immune.institute>
2023-02-14 17:33:00 -0400

    añadir fromas de pago y modificacion de base

--- VendingMachine/Vending_Machine/views.py
+++ VendingMachine/Vending_Machine/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Producto
 
 def index(request):
     productos = Producto.objects.all()
-    return render(request, 'index.html', {'productos': productos})
+    context = {'productos': productos}
+    return render(request, 'index.html', context)
+
+def pagar(request):
+    if request.method == 'POST':
+        codigo_producto = request.POST['codigoDeProducto']
+        producto = Producto.objects.get(codigo_producto=codigo_producto)
+        producto.stock -= 1
+        producto.numero_comprados += 1
+        producto.save()
+        return redirect('index')
+    return redirect('index')
